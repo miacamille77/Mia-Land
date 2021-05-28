5c0a48dfcd6b09ea4e2f4b67a75c1da287f5da06
miacamille77 <77417036+miacamille77@users.noreply.github.com>
2021-05-28 19:14:12 -0400

    Create jokebot.js

---
+++ jokebot.js
@@ -0,0 +1,74 @@
+var answer = "A pork chop!";
+var guess;
+
+var jokes = ["What do you call a pig that does karate?", "What do you call a fake noodle?, why was 6 afraid of 9 on new years eve?"]
+
+function startJoke(){
+  if(confirm("Wanna hear a joke?")){
+    {
+      
+    }
+  };
+  
+    
+  
+
+
+   
+    
+    
+    
+    // if I say ok, start the joke
+    function tellJoke() {
+      
+    } guess = prompt("What do you call a pig that does karate?");
+    //once I answer, check if I'm right
+    if(guess === answer){
+  //if I'm right, congrats
+  alert ("You got it! Great job.");
+} else {
+  alert ("wrong.")
+  alert (answer);
+  
+} 
+  
+} 
+
+
+  
+
+
+
+  ("What do you call a fake noodle?");
+ // prompt ("oops wrong!!");
+//  alert ("Impasta!");
+ // prompt ("Wait! I have another joke");
+//  prompt ("Why was 6 afraid of 9 one new years eve?");
+ // prompt ("Wrong!");
+ // prompt ("Because 9,8,7");
+//  prompt ("Hold up!! You think I am done with the jokes noo, who do you think I am?!");
+//  prompt ("Look one last joke & I won't bother you?");
+//  prompt ("Why do bees have sticky hair?");
+ // prompt ("Wrong again!");
+//  alert ("Because they use honeycombs!!");
+
+// if I say ok, start the joke
+
+
+// if I say no, do noting 
+
+//once I answer, check if I'm right
+
+  
+
+
+
+
+
+
+
+//if I'm wrong, tell answer 
+
+
+ 
+// alert ();
